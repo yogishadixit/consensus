1cc3f18682a8ad3cba5afdf3eda8091f6dae4262
Yogisha Dixit <yad4@cornell.edu>
2016-03-11 01:33:55 -0500

    Basic scalatra project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-Consensus
-====
+# consensus #
+
+## Build & Run ##
+
+```sh
+$ cd consensus
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/github/yogishadixit/consensus/ConsensusServlet.scala
@@ -0,0 +1,16 @@
+package com.github.yogishadixit.consensus
+
+import org.scalatra._
+
+class ConsensusServlet extends ConsensusStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
