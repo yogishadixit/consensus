6de97a34caffdd528e8db0af5563e6792183ba0b
Yogisha Dixit <yad4@cornell.edu>
2016-03-11 02:31:59 -0500

    Create suggestion and poll models

---
+++ src/main/scala/com/github/yogishadixit/consensus/models/Poll.scala
@@ -0,0 +1,14 @@
+package com.github.yogishadixit.consensus.models
+
+import java.sql.Timestamp
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by yogishadixit on 3/11/16.
+  */
+case class Poll(creator: User,
+                timestamp: Timestamp,
+                users: ListBuffer[User],
+                suggestions: ListBuffer[Suggestion],
+                consensus: Suggestion)
